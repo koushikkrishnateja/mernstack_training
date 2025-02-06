fe4de93b6e86498ab09dde874688be89e45b6b01
koushikkrishnateja <2211cs010519@mallareddyuniversity.ac.in>
2025-02-06 11:39:07 +0530

    commit your message

--- Documents/Desktop/koushik_Reactapp/my_react_app/src/App.js
+++ Documents/Desktop/koushik_Reactapp/my_react_app/src/App.js
@@ -1,73 +1,26 @@
-// import React from "react";
-// import './App.css';
+// import React from 'react';
+// import PropsChildrenex from './propexample/PropsChildrenex';
+// import SubChildProps from './propexample/SubChildProps';
 
-// function App() {
-//   // let x = React.createElement("div",{classNAme:"App"},
-//   //   React.createElement("h1",null,"Welcome to React")
-//   // )
-//   let username="Koushik";
-//     return (
-//   //   <div className="App">
-//   //     <h1>Welcome to React</h1>
-//   //      <img/>
-//   //   </div>
-//     // x
-//     <div> 
-//       <h1>ExcleR</h1>
-//       <p>Super</p>
-//       {username}
-//       {777}
-//     </div>
-
-//      );
-// }
-
-// export default App;
-// import React from "react";
-// class App extends React.Component{
-//   render(){
-//     return(
-//       <div>
-//         <h1>Class Component</h1>
-//       </div>
-//     )
-//   }
-
-// }
-// export default App;
-
-
-// function App(){
-//   return(
-//     <div>
-//       <h1>Functional component</h1>
-//     </div>
-//   )
-// }
-// export default App;
-
-// const App=( )=>{
+// const App = () => {
 //   return (
 //     <div>
-//       <h1>Functional component</h1>
+//       <PropsChildrenex username="koushik" company="Meta">
+//         <h1>This data is passing as a props children to child component</h1>
+//         <SubChildProps child1="Child Prop 1" child2="Child Prop 2" />
+//       </PropsChildrenex>
 //     </div>
-//   )
+//   );
 // }
-// export default App();
-import React,{Component} from "react";
-import CBCPropEx1 from "./components/propexample/CBCpropEx1";
-export default class App extends Component{
-    render(){
-        return(
-            <div>
-               App
-               <CBCPropEx1
-               username="Uday"
-               age={19}
-               hobbies={["playing","Eating","Sleeping"]}
-               address={{city : "Hyderabad",arae : "maisammaguda"}}
-               sendFun={function(){alert("Hi koushik")}}/>
-               </div>  
-                     )
-    }
+
+// export default App;
+import React from 'react';
+import CBCStateEx from './propexample/CBCPropEX1';
+const App=()=>{
+  return(
+    <div>
+      <CBCStateEx/>
+    </div>
+  );
 }
+export default App;
